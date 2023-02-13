6dc4082837cf13e46fe88a415d4ab271da194f8f
Vladimir <dr.frisbi@gmail.com>
2023-02-13 20:58:13 +0700

    add code task

--- test/Program.cs
+++ test/Program.cs
@@ -7,3 +7,42 @@
 // ["1234", "1567", "-2", "computer science"] -> ["2"]
 // ["Russia", "Denmark", "Kazan"] -> []
 
+
+string[] CreateArray()
+{
+    Console.WriteLine("Введите количество элементов массива ");
+    int size = Convert.ToInt32(Console.ReadLine());
+    string[] array1 = new string[size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.WriteLine($"Введите {i + 1} элемент массива");
+        array1[i] = Convert.ToString(Console.ReadLine());
+    }
+    return array1;
+}
+
+void SecondArrayWithIF(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+string[] array1 = CreateArray();
+string[] array2 = new string[array1.Length];
+PrintArray(array2);
+SecondArrayWithIF(array1, array2);
+PrintArray(array2);
